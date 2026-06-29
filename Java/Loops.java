4235d60cefac2ef9abe8661f80031679052bb15f
Nakshatra2410 <maanasa.23bce9549@vitapstudent.ac.in>
2026-06-29 18:43:07 +0530

    Day-3 java

---
+++ Java/Loops.java
@@ -0,0 +1,37 @@
+public class Loops {
+    public static void main(String[] args) {
+        // while loop
+
+        // int i=1;
+        // while (i<=4) {
+        //     System.out.println("Hi "+i);
+        //     int j=1;
+        //     while(j<=3)
+        //     {
+        //         System.out.println("Hello");
+        //         j++;
+        //     }
+        //     i++;
+        // }
+        // System.out.println("Bye "+i);
+
+        //do-while loop
+
+        // int i=5;
+        // do{
+        //     System.out.println("Hi "+i);
+        //     i++;
+        // }while(i<=4);
+
+        //for loop
+
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("DAY "+i);
+            for(int j=1;j<=9;j++)
+            {
+                System.out.println("  "+(j+8)+" - "+(j+9));
+            }
+        }
+    }
+}
--- Java/Practice.java
+++ Java/Practice.java
@@ -125,17 +125,123 @@ public class Practice {
         //     System.out.println("F");
 
         //Simple login (uname,password using if)
-        Scanner sc = new Scanner(System.in);
-        String cuname = "admin";
-        String copas = "java123";
-        String uname = sc.nextLine();
-        String pas = sc.nextLine();
-        if(uname.equals(cuname) && pas.equals(copas))
-        {
-            System.out.println("Login successful");
-        }
-        else{
-            System.out.println("Login failed. Please try again");
-        }
+        // Scanner sc = new Scanner(System.in);
+        // String cuname = "admin";
+        // String copas = "java123";
+        // String uname = sc.nextLine();
+        // String pas = sc.nextLine();
+        // if(uname.equals(cuname) && pas.equals(copas))
+        // {
+        //     System.out.println("Login successful");
+        // }
+        // else{
+        //     System.out.println("Login failed. Please try again");
+        // }
+
+        //Multiplication table
+        // for(int i=1;i<=10;i++)
+        // {
+        //     System.out.println("2 x "+i+" = "+2*i);
+        // }
+
+        //Factorial
+        // int n=5;
+        // int fact = n;
+        // for(int i=n-1;i>=1;i--)
+        // {
+        //     fact *= i;
+        // }
+        // System.out.println(fact);
+
+        //Fibonacci series
+        // int a = 0;
+        // int b = 1;
+        // int n = 5;
+        // for(int i=0;i<n;i++)
+        // {
+        //     System.out.print(a+" ");
+        //     int c = a+b;
+        //     a = b;
+        //     b = c;
+        // }
+
+        //Prime number
+        // int n = 100;
+        // boolean flag = true;
+        // for(int i=2;i*i<=n;i++)
+        // {
+        //     if(n%i==0)
+        //     {
+        //         flag = false;
+        //     }
+        // }
+        // if(flag)
+        //     System.out.println("Prime");
+        // else
+        //     System.out.println("Not prime");
+
+        //reverese a no
+        // int n = 1234;
+        // int rev = 0;
+        // while(n>0)
+        // {
+        //     rev = rev*10+(n%10);
+        //     n /= 10;
+        // }
+        // System.out.println(rev);
+
+        //sum of digits
+        // int n = 1234;
+        // int sum = 0;
+        // while(n>0)
+        // {
+        //     int digit = n%10;
+        //     sum += digit;
+        //     n /= 10;
+        // }
+        // System.out.println(sum);
+
+        //palindrome no
+        // int n = 132;
+        // int o = n;
+        // int rev = 0;
+        // while(n>0)
+        // {
+        //     rev = rev*10+(n%10);
+        //     n /= 10;
+        // }
+        // if(rev==o)
+        // {
+        //     System.out.println("Palindrome");
+        // }
+        // else{
+        //     System.out.println("Not a palindrome");
+        // }
+
+        //count digits
+        // int n = 123;
+        // int count = 0;
+        // while(n>0)
+        // {
+        //     count++;
+        //     n/=10;
+        // }
+        // System.out.println(count);
+
+        //pattern 
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+        // int n = 5;
+        // for(int i=1;i<=5;i++)
+        // {
+        //     for(int j=1;j<=i;j++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
